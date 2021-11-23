c771b250ae3abdcfd024941e8e63645163989377
@andriy.trofimov <trofimov.a@hanaboso.com>
2021-11-23 17:49:25 +0200

    PLaying around

--- src/script.js
+++ src/script.js
@@ -3,18 +3,19 @@ const btnPlay = document.getElementById('play');
 const btnStop = document.getElementById('stop');
 const input = document.getElementById('input');
 let inputValue = ''
+let simpleLoop = null
 Tone.context.resume();
 
 btnStart.addEventListener('click', () => {
     Tone.context.resume().then(() => Tone.Transport.start());
 });
-const simpleLoop = scribble.clip({ synth: 'Synth', notes: 'c4', pattern: 'x' });
+
 
 input.addEventListener('input', (event) =>
 {
   inputValue = event.target.value
   console.log(event.target.value, 'INput value', inputValue);
-  return inputValue
+  simpleLoop = scribble.clip({ synth: 'Synth', notes: inputValue, pattern: 'x' });
 })
 btnPlay.addEventListener('click', () =>
 {
